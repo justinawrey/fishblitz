3b4009ab2beb108f9bdd44860281d0e7179abf6e
Justin Awrey <awreyjustin@gmail.com>
2023-11-12 16:08:13 +0200

    ui placeholders

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -1,7 +1,36 @@
+using TMPro;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    private int _rods = 1;
-    private bool _hasKey = false;
+    private Reactive<int> _rods = new Reactive<int>(1);
+    private Reactive<int> _money = new Reactive<int>(0);
+    private Reactive<bool> _hasKey = new Reactive<bool>(false);
+
+    [Header("UI hookups")]
+    [SerializeField] private TextMeshProUGUI _rodUiCount;
+    [SerializeField] private TextMeshProUGUI _moneyUiCount;
+
+    private void Start()
+    {
+        UpdateRodUi(_rods.Get());
+        UpdateMoneyUi(_money.Get());
+        HookUpUi();
+    }
+
+    private void HookUpUi()
+    {
+        _rods.OnChange((prev, curr) => UpdateRodUi(curr));
+        _money.OnChange((prev, curr) => UpdateMoneyUi(curr));
+    }
+
+    private void UpdateRodUi(int to)
+    {
+        _rodUiCount.text = $"rods: {to}";
+    }
+
+    private void UpdateMoneyUi(int to)
+    {
+        _moneyUiCount.text = $"money: {to}";
+    }
 }
