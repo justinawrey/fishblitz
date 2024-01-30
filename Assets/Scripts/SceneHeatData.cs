8c5a4818b209bce809959dee5a3e54732d63fb92
coalstevens <62672483+coalstevens@users.noreply.github.com>
2024-01-30 18:43:42 +0700

    Reorganized script locations

--- Assets/Scripts/SceneHeatData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SceneHeatData : ScriptableObject
-{
-    public int State; 
-    public List<int> Counters = new List<int>();
-}
