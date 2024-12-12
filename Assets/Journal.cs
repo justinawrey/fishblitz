3fc13ec3ebdb94ed684bb5003f6591266a34ed58
coalstevens <62672483+coalstevens@users.noreply.github.com>
2024-12-11 19:52:00 -0700

    Journal interface improvements.

--- Assets/Journal.cs
+++ Assets/Journal.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -8,28 +10,26 @@ using UnityEngine.UI;
 
 public class Journal : MonoBehaviour
 {
-    [System.Serializable]
-    private class EntryIcons
-    {
-        public string Name;
-        public GameObject BirdIcon;
-        public GameObject QuestionMarkIcon;
-    }
-
     [Range(0f, 1f)][SerializeField] private float solidOpacity = 1.0f;
     [Range(0f, 1f)][SerializeField] private float fadedOpacity = 0.3f;
+    [Header("Pages")]
+    [SerializeField] private Transform _leftPage;
     [SerializeField] private Transform _cursor;
-    [SerializeField] private float _cursorRepeatIntervalSecs = 0.4f;
+    [SerializeField] private Transform _rightPage;
+    [Header("NoteBook")]
+    [SerializeField] private TextMeshProUGUI _noteBookTitle;
     [SerializeField] private List<Image> _seasonIcons = new();
     [SerializeField] private List<Image> _dayPeriodIcons = new();
-    [SerializeField] private List<EntryIcons> _journalEntries = new();
-    [SerializeField] private TextMeshProUGUI _noteBookTitle;
+    [SerializeField] private Image _numerator;
+    [SerializeField] private Image _denominator;
+    [SerializeField] private List<Sprite> _numbersTo24RightJustified = new();
+    [SerializeField] private List<Sprite> _numbersTo24LeftJustified = new();
     private Dictionary<string, PlayerData.BirdingLogEntry> _playerBirdLogLookup;
-    private float _lastCursorMoveTime = 0;
-    private int _activeEntryIndex = 0;
-    private int _numRows = 8;
-    private int _numColumns = 3;
-
+    private Transform[][] _pagesLookup;
+    private (int i, int j) _pagePointer;
+    private int _numRows = 4;
+    private int _numColumns = 6;
+    private int _totalEntries;
 
     void Start()
     {
@@ -37,92 +37,132 @@ public class Journal : MonoBehaviour
             Debug.LogError("Season icons are assigned incorrectly in the journal.");
         if (_dayPeriodIcons.Count != 4)
             Debug.LogError("Day period icons are assigned incorrectly in the journal.");
-        _playerBirdLogLookup = PlayerData.Instance.PlayerBirdingLog.CaughtBirds.ToDictionary(b => b.Name, b => b);
+        InitializePlayerBirdLogLookup();
+        var _allEntries = CollectActiveEntries();
+        _totalEntries = _allEntries.Count;
+        InitializePagesLookupTable(_allEntries);
+        RefreshPages(PlayerData.Instance.PlayerBirdingLog);
+        UpdateNoteBook();
+        StartCoroutine(UpdateCursorAfterDelay());
+    }
 
-        UpdateJournal(PlayerData.Instance.PlayerBirdingLog);
-        UpdateNoteBookInfo();        
+    private IEnumerator UpdateCursorAfterDelay() {
+        yield return null;
+        UpdateCursorPosition();
+    }
+
+    private void UpdateCursorPosition() {
+        _cursor.position = _pagesLookup[_pagePointer.i][_pagePointer.j].GetChild(1).transform.position;
+    }
+
+    private void InitializePlayerBirdLogLookup()
+    {
+        _playerBirdLogLookup = PlayerData.Instance.PlayerBirdingLog.CaughtBirds.ToDictionary(b => b.Name, b => b);
+    }
+
+    private List<Transform> CollectActiveEntries() {
+        List<Transform> _allEntries = new();
+        foreach (Transform child in _leftPage)
+            if(child.gameObject.activeSelf)
+                _allEntries.Add(child);
+        foreach (Transform child in _rightPage)
+            if(child.gameObject.activeSelf)
+                _allEntries.Add(child);
+        return _allEntries;
+    }
+
+    private void InitializePagesLookupTable(List<Transform> allEntries)
+    {
+        _pagePointer = (0,0);
+        _pagesLookup = new Transform[_numRows][];
+        for (int i = 0; i < _numRows; i++)
+            _pagesLookup[i] = new Transform[_numColumns];
+        
+        if (_totalEntries > _numColumns * _numRows)
+            Debug.LogError("Too many entries in journal.");
+
+        int k = 0;
+        // Map entries to left page
+        for (int i = 0; i < _numRows; i++)
+        {
+            for (int j = 0; j < _numColumns / 2; j++)
+            {
+                if (k >= _totalEntries)
+                    return;
+                _pagesLookup[i][j] = allEntries[k];
+                k++;
+            }
+        }
+        // right page
+        for (int i = 0; i < _numRows; i++)
+        {
+            for (int j = _numColumns / 2; j < _numColumns; j++)
+            {
+                if (k >= _totalEntries)
+                    return;
+                _pagesLookup[i][j] = allEntries[k];
+                k++;
+            }
+        }
     }
 
     public void OnMoveCursor(InputValue value)
     {
-        // if (Time.time - _lastCursorMoveTime < _cursorRepeatIntervalSecs)
-        //     return;
-
-        int _cursorMove = GetCursorMove(value.Get<Vector2>()); 
-        if (_cursorMove != 0)
-        {
-            // _lastCursorMoveTime = Time.time;
-            _activeEntryIndex += _cursorMove;
-            UpdateNoteBookInfo();
+        if (TryMoveJournalPointer(value.Get<Vector2>())) {
+            UpdateCursorPosition();
+            UpdateNoteBook();
         }
     }
 
-    private void UpdateNoteBookInfo() {
-            _cursor.position = _journalEntries[_activeEntryIndex].BirdIcon.transform.position;
-            // get the entry for the selected bird from the player log if it exists
-            UpdateNoteBookInfo(_playerBirdLogLookup.TryGetValue(_journalEntries[_activeEntryIndex].Name, out var entry) ? entry : null);
+    private void UpdateNoteBook()
+    {
+        // get the entry for the selected bird from the player log if it exists
+        string _birdName = _pagesLookup[_pagePointer.i][_pagePointer.j].gameObject.name;
+        RefreshNotebook(_playerBirdLogLookup.TryGetValue(_birdName, out var entry) ? entry : null);
     }
 
-    private int GetCursorMove(Vector2 inputDirection) {
-        // Mapping cursor movement of a 1D list of entries to a numRows x numColumns matrix
-        // move up
-        if (inputDirection.y == 1 && _activeEntryIndex - _numColumns >= 0)
-            return -_numColumns;
-        // move down
-        else if (inputDirection.y == -1 && _activeEntryIndex + _numColumns < _journalEntries.Count)
-            return _numColumns;
-        else if (inputDirection.x == 1 && _activeEntryIndex + 1 < _journalEntries.Count)
-        {
-            // move right on right page. Can't if in last column
-            if (_activeEntryIndex >= _numColumns * _numRows && (_activeEntryIndex + 1) % _numColumns != 0)
-                return 1;
-            // move right on left page. Jump to right page if in last column
-            else
-            {
-                int _jumpDistance = _numColumns * _numRows - (_numColumns - 1);
-                if ((_activeEntryIndex + 1) % 3 == 0 && _activeEntryIndex + _jumpDistance < _journalEntries.Count)
-                    return _jumpDistance;
-                else
-                    return 1;
-            }
-        }
-        else if (inputDirection.x == -1 && _activeEntryIndex - 1 >= 0)
-        {
-            // move left on right page. Jump to left page if in first column
-            if (_activeEntryIndex >= _numColumns * _numRows)
-            {
-                int _jumpDistance = _numColumns * _numRows - (_numColumns - 1);
-                if (_activeEntryIndex % 3 == 0 && _activeEntryIndex - _jumpDistance >= 0)
-                    return  -_jumpDistance;
-                else if (_activeEntryIndex - 1 >= 0)
-                    return -1;
-            }
-            // move left on left page. Can't if in first column.
-            else if (_activeEntryIndex - 1 >= 0 && _activeEntryIndex % 3 != 0)
-                return -1;
-        }
-        return 0;
+    private bool TryMoveJournalPointer(Vector2 inputDirection)
+    {
+        int newRow = _pagePointer.i + (int)inputDirection.y * -1;
+        int newCol = _pagePointer.j + (int)inputDirection.x;
+
+        if (newRow < 0 || newRow >= _numRows || newCol < 0 || newCol >= _numColumns)
+            return false;
+
+        if (_pagesLookup[newRow][newCol] == null)
+            return false;
+
+        _pagePointer.i = newRow;
+        _pagePointer.j = newCol;
+        return true;
     }
 
-    private PlayerData.BirdingLogEntry GetEntryOfBird(string birdname, PlayerData.BirdingLog birdingLog)
+    private PlayerData.BirdingLogEntry GetBirdLogEntry(string birdname, PlayerData.BirdingLog birdingLog)
     {
         return _playerBirdLogLookup.TryGetValue(birdname, out var entry) ? entry : null;
     }
 
-    private void UpdateJournal(PlayerData.BirdingLog birdingLog)
+    private void RefreshPages(PlayerData.BirdingLog birdingLog)
     {
-        foreach (var _bird in birdingLog.CaughtBirds)
+        _numerator.sprite = _numbersTo24RightJustified[_playerBirdLogLookup.Count];
+        _denominator.sprite = _numbersTo24LeftJustified[_totalEntries];
+
+        foreach (Transform _child in _leftPage)
         {
-            foreach (var _entry in _journalEntries)
-            {
-                bool _birdCaught = _entry.Name == _bird.Name;
-                _entry.BirdIcon.SetActive(_birdCaught);
-                _entry.QuestionMarkIcon.SetActive(!_birdCaught);
-            }
+            bool _isBirdInLog = _playerBirdLogLookup.ContainsKey(_child.gameObject.name);
+            _child.GetChild(0).gameObject.SetActive(!_isBirdInLog); // Set ? Icon
+            _child.GetChild(1).gameObject.SetActive(_isBirdInLog); // Set bird Icon
+        }
+
+        foreach (Transform _child in _rightPage)
+        {
+            bool _isBirdInLog = _playerBirdLogLookup.ContainsKey(_child.gameObject.name);
+            _child.GetChild(0).gameObject.SetActive(!_isBirdInLog); // Set ? Icon
+            _child.GetChild(1).gameObject.SetActive(_isBirdInLog); // Set bird Icon
         }
     }
 
-    private void UpdateNoteBookInfo(PlayerData.BirdingLogEntry selectedEntry)
+    private void RefreshNotebook(PlayerData.BirdingLogEntry selectedEntry)
     {
         if (selectedEntry == null)
         {
